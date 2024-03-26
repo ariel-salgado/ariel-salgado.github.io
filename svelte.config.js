d4954c5c744a8494e4acfdb2b4875db30c2851fc
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-03-26 04:06:05 -0300

    Trying to fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -16,14 +14,9 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
 			precompress: true,
 			strict: true
 		}),
-
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH
-		}
 	},
 
 	compilerOptions: {

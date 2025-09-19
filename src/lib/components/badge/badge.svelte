6a3b3b01c694289c1696f47851669fdd529c4da7
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-09-19 02:10:28 -0300

    style: ensure children is centered

--- src/lib/components/badge/badge.svelte
+++ src/lib/components/badge/badge.svelte
@@ -11,6 +11,6 @@
 	let { children, class: class_name }: Props = $props();
 </script>
 
-<div class={cn('h-7 content-center rounded-4xl bg-accent px-3 text-accent-foreground', class_name)}>
-	<span class='content-center text-sm font-semibold'>{@render children()}</span>
+<div class={cn('h-6 content-center rounded-4xl bg-accent px-3 text-accent-foreground', class_name)}>
+	<span class='flex items-center text-sm font-semibold'>{@render children()}</span>
 </div>

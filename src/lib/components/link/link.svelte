8d29d1498d84c18723c3e41e100e44a3bc71a9ba
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-09-24 02:02:21 -0300

    style: change flex for inline-flex to avoid new line

--- src/lib/components/link/link.svelte
+++ src/lib/components/link/link.svelte
@@ -11,6 +11,6 @@
 	let { children, class: class_name, ...rest }: Props = $props();
 </script>
 
-<a class={cn('flex w-fit items-center gap-x-1 focus-within:underline hover:underline', class_name)} referrerpolicy='no-referrer' {...rest}>
+<a class={cn('inline-flex w-fit items-center gap-x-1 focus-within:underline hover:underline', class_name)} referrerpolicy='no-referrer' {...rest}>
 	{@render children()}
 </a>

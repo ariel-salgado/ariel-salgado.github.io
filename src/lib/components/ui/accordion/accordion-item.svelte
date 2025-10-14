a99659f8f5d0507b5507f823b6ee0bd15bf43347
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-10-14 19:50:44 -0300

    feat: added accordion item component

---
+++ src/lib/components/ui/accordion/accordion-item.svelte
@@ -0,0 +1,33 @@
+<script lang='ts'>
+	import type { Snippet } from 'svelte';
+
+	import Accordion from './accordion.svelte';
+
+	interface Props {
+		class?: string;
+		title: string;
+		subtitle?: string;
+		children: Snippet;
+	}
+
+	let { title, subtitle, children, class: class_name }: Props = $props();
+</script>
+
+<Accordion class={class_name}>
+	{#snippet header()}
+		<hgroup class='w-full'>
+			<h3 class='pb-1 text-2xl font-medium text-primary'>
+				{title}
+			</h3>
+			<p class='text-muted-foreground'>
+				{subtitle}
+			</p>
+		</hgroup>
+	{/snippet}
+
+	{#snippet content()}
+		<section class='flex flex-col gap-y-6 py-2'>
+			{@render children()}
+		</section>
+	{/snippet}
+</Accordion>

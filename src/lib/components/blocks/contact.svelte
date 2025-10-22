14304465a299b3367d1f48c46184f39d133c9cbc
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-10-22 17:13:24 -0300

    refactor: turn contact into a block component

---
+++ src/lib/components/blocks/contact.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+	import { FloatingWindow } from '../ui';
+
+	interface Props {
+		open: boolean;
+	}
+
+	let { open = $bindable(false) }: Props = $props();
+</script>
+
+<FloatingWindow bind:open>
+	{#snippet header()}
+		<h2 class='text-xl font-medium text-foreground'>contacto</h2>
+	{/snippet}
+	{#snippet content()}
+		<section>henlo</section>
+	{/snippet}
+</FloatingWindow>
--- src/lib/components/blocks/index.ts
+++ src/lib/components/blocks/index.ts
@@ -1,4 +1,5 @@
 export { default as Faq } from './faq.svelte';
+export { default as Contact } from './contact.svelte';
 export { default as AboutMe } from './about-me.svelte';
 export { default as Projects } from './projects.svelte';
 export { default as Experience } from './experience.svelte';
